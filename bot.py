3dc8b72629e45f3431ad37fe75ca682a55dc4273
ramdev-git <65360179+ramdev-git@users.noreply.github.com>
2020-10-14 20:44:52 +0300

    firt

---
+++ bot.py
@@ -0,0 +1,14 @@
+import telebot
+
+bot = telebot.TeleBot("1348161215:AAHLT_cdj2657dPdKG0CmZAesqbRL6DHP94")
+
+@bot.message_handler(commands=["start"])
+def send_welcome(message):
+    bot.send_message(message.chat.id, 'Привет! Вас приветствует Mafia Bonds')
+
+@bot.message_handler(commands=["rules", "Правила"])
+def send_rules(message):
+    bot.send_message(message.chat.id, 'Правила1')
+
+if __name__ == "__main__":    
+    bot.polling()
