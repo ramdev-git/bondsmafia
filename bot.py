7f3c592f198c13de1fdfd48d98d3aed450919963
Ram2020 <65360179+ramdev-git@users.noreply.github.com>
2020-10-15 17:40:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,8 +10,10 @@ def send_welcome(message):
 def handle_docs_audio(message):
 	bot.send_message(message.chat.id, 'Привет')
     
-text = '[<Правила>](<https://telegra.ph/Pravila-igry-09-08-4>)'
-bot.send_message(message.chat.id, text, parse_mode='MarkdownV2')
+@bot.message_handler(commands=["rules", "Правила"])
+def send_rules(message):
+    link = '[Правила чата](https://telegra.ph/Pravila-igry-09-08-4)'
+    bot.send_message(message.chat.id, link, parse_mode='MarkdownV2')
 
 @bot.message_handler(commands=["help", "Справка"])
 def send_rules(message):
