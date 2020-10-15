e5a454a6ae6c69e7049287d926822d2e9ad7e911
Ram2020 <65360179+ramdev-git@users.noreply.github.com>
2020-10-15 19:31:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,9 @@
 import telebot
 from time import time
+from time import sleep
+import config
+import logging
+import datetime
 bot = telebot.TeleBot("1214001983:AAE-VmGC4SK6bGg4nXUvHtlm2ArsPKWsG1s")
 '''
 bot = telebot.TeleBot("1348161215:AAHLT_cdj2657dPdKG0CmZAesqbRL6DHP94")
@@ -77,6 +81,69 @@ def set_ro(message):
     bot.restrict_chat_member(message.chat.id, message.from_user.id, until_date=time()+600)
     bot.send_message(message.chat.id, strings.get(get_language(message.from_user.language_code)).get("ro_msg"),
                      reply_to_message_id=message.message_id)
+
+'''
+------------------------------------------------------------
+'''
+
+@bot.message_handler(commands=["ping"]) #Создаем команду
+def start(message):
+	try: #Заворачиваем все в try
+		bot.send_message(message.chat.id, "<b>PONG!</b>" , parse_mode="HTML") 
+		bot.forward_message(config.owner, message.chat.id, message.message_id)
+	except:
+		bot.send_message(config.owner, 'Что-то пошло не так!') #анная система (оборачивание в try и except позволит продолжить выполнение кода, даже если будут ошибки)
+
+		
+
+
+
+
+
+
+
+@bot.message_handler(commands=['send'])
+def process_start(message):
+	if int(message.chat.id) == config.owner:
+		try:
+			bot.send_message(message.chat.id, 'Для отправки сообщения сделай реплей')
+			bot.forward_message(config.owner, message.chat.id, message.message_id)
+			bot.register_next_step_handler(message, process_mind)
+		except:
+			bot.send_message(message.chat.id, "Что-то пошло не так! Ошибка возникла в блоке кода:\n<code>@bot.message_handler(commands=['send_message'])</code>", parse_mode='HTML')
+	else:
+		bot.send_message(message.chat.id, 'Вы не являетесь администратором для выполнения этой команды!')
+
+
+def process_mind(message):
+	if int(message.chat.id) == config.owner:
+		try:
+			text = 'Сообщение было отправлено пользователю ' + str(message.reply_to_message.forward_from.first_name)
+			bot.forward_message(message.reply_to_message.forward_from.id, config.owner, message.message_id)
+			bot.send_message(config.owner, text)
+		except:
+			bot.send_message(message.chat.id, 'Что-то пошло не так! Бот продолжил свою работу.' + ' Ошибка произошла в блоке кода:\n\n <code>def process_mind(message)</code>', parse_mode='HTML')
+	else:
+		bot.send_message(message.chat.id, 'Вы не являетесь администратором для выполнения этой команды!')
+
+
+@bot.message_handler(content_types=["text"])
+def messages(message):
+	if int(message.chat.id) == config.owner:
+		try:
+			bot.send_message(message.chat.id, 'Сообщение от администратора было получено')
+		except:
+			bot.send_message(config.owner, 'Что-то пошло не так! Бот продолжил свою работу.' + ' Ошибка произошла в блоке кода:\n\n <code>@bot.message_handler(content_types=["text"])</code>', parse_mode='HTML')
+	else:
+		pass
+		try:
+			bot.forward_message(config.owner, message.chat.id, message.message_id)
+			bot.send_message(message.chat.id, str(message.from_user.first_name) + ',' +' я получил сообщение и очень скоро на него отвечу :)')
+		except:
+			bot.send_message(config.owner, 'Что-то пошло не так! Бот продолжил свою работу.')
+		
+		
+
 
 
 if __name__ == "__main__":    
