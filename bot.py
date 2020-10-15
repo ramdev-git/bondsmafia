16c2060fa1a3519c25418e1d6c1e80750e35e2ad
Ram2020 <65360179+ramdev-git@users.noreply.github.com>
2020-10-15 17:52:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ def send_welcome(message):
     bot.send_message(message.chat.id, 'Привет! Вас приветствует Mafia Bonds')
 @bot.message_handler(content_types=['new_chat_members'])
 def handle_docs_audio(message):
-	bot.send_message(message.chat.id, 'Привет,' 'username')
+    bot.send_message(message.chat.id, "Добро пожаловать, {0}!".format(user_name))
     
 @bot.message_handler(commands=["rules", "Правила"])
 def send_rules(message):
