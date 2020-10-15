15736fbbb6ebe4cb1a269151647eddcd7d086025
Ram2020 <65360179+ramdev-git@users.noreply.github.com>
2020-10-15 18:21:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,10 +14,17 @@ def handle_docs_audio(message):
 def send_rules(message):
     link = '[Правила чата](https://telegra.ph/Pravila-igry-09-08-4)'
     bot.send_message(message.chat.id, link, parse_mode='MarkdownV2')
-
+    
+@bot.message_handler(commands=["FAQ", "Как играть])
+def send_rules(message):
+    link = '[Как играть? -> Посмотрите в этой статье <-](https://telegra.ph/FAQ-po-igre-v-Mafiyu-09-23)'
+    bot.send_message(message.chat.id, link, parse_mode='MarkdownV2')
+)'
+                               
 @bot.message_handler(commands=["help", "Справка"])
 def send_rules(message):
     bot.send_message(message.chat.id, 'Доступные команды:\n /rules\n  ')
 
 if __name__ == "__main__":    
     bot.polling()
+https://telegra.ph/FAQ-po-igre-v-Mafiyu-09-23
