564c4627d78a8610e88dd5e6165e5354e2f59e0e
Ram2020 <65360179+ramdev-git@users.noreply.github.com>
2020-10-15 17:12:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,10 @@ bot = telebot.TeleBot("1348161215:AAHLT_cdj2657dPdKG0CmZAesqbRL6DHP94")
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
     bot.send_message(message.chat.id, 'Привет! Вас приветствует Mafia Bonds')
-
+@bot.message_handler(content_types=['new_chat_members'])
+def handle_docs_audio(message):
+	bot.send_message(message.chat.id, 'Привет')
+    
 @bot.message_handler(commands=["rules", "Правила"])
 def send_rules(message):
     bot.send_message(message.chat.id, 'https://telegra.ph/Pravila-igry-09-08-4')
