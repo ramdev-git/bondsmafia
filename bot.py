82bcb40fef70c54953bf69db2e083e8309f40152
Ram2020 <65360179+ramdev-git@users.noreply.github.com>
2020-10-16 20:09:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import telebot
 from time import time
+from telebot import types
 
 bot = telebot.TeleBot("1214001983:AAE-VmGC4SK6bGg4nXUvHtlm2ArsPKWsG1s")
 '''
@@ -10,10 +11,10 @@ def send_welcome(message):
     bot.send_message(message.chat.id, 'Привет! Вас приветствует Mafia Bonds')
 @bot.message_handler(content_types=['new_chat_members'])
 def handle_docs_audio(message):
-      keyboard = types.InlineKeyboardMarkup()
-      url_button = types.InlineKeyboardButton(text="Правила", url="https://telegra.ph/Pravila-igry-09-08-4")
-      keyboard.add(url_button)
-      bot.send_message(message.chat.id, "🙋Добро пожаловать, {0.first_name}!\n⚙️Доступные команды можно узнать через /help", reply_markup=keyboard.format(message.from_user, bot.get_me()))
+	keyboard = types.InlineKeyboardMarkup()
+	btn_url = types.InlineKeyboardButton(text="Правила чата", url="https://telegra.ph/Pravila-igry-09-08-4")
+	keyboard.add(btn_url)
+      bot.send_message(message.chat.id, "🙋Добро пожаловать, {0.first_name}!\n⚙️Доступные команды можно узнать через /help".format(message.from_user, bot.get_me())reply_markup=keyboard)
     
 @bot.message_handler(commands=["rules"])
 def send_rules(message):
