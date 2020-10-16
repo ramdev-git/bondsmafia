2cf4589433234cb3738dff32317939f0f0300162
Ram2020 <65360179+ramdev-git@users.noreply.github.com>
2020-10-16 20:34:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ bot = telebot.TeleBot("1348161215:AAHLT_cdj2657dPdKG0CmZAesqbRL6DHP94")
 '''
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
-    	bot.send_message(message.chat.id, 'Привет! Вас приветствует Mafia Bonds')
+    bot.send_message(message.chat.id, 'Привет! Вас приветствует Mafia Bonds')
 @bot.message_handler(content_types=['new_chat_members'])
 def handle_docs_audio(message):
     keyboard = types.InlineKeyboardMarkup()
@@ -18,28 +18,28 @@ def handle_docs_audio(message):
     
 @bot.message_handler(commands=["rules"])
 def send_rules(message):
-    	link = '[Правила чата](https://telegra.ph/Pravila-igry-09-08-4)'
-    	bot.send_message(message.chat.id, link, parse_mode='MarkdownV2')
+    link = '[Правила чата](https://telegra.ph/Pravila-igry-09-08-4)'
+    bot.send_message(message.chat.id, link, parse_mode='MarkdownV2')
     
 @bot.message_handler(commands=["faq"])
 def send_rules(message):
-    	link = '[Как играть? Посмотрите в этой статье](https://telegra.ph/FAQ-po-igre-v-Mafiyu-09-23)'
-    	bot.send_message(message.chat.id, link, parse_mode='MarkdownV2')
+    link = '[Как играть? Посмотрите в этой статье](https://telegra.ph/FAQ-po-igre-v-Mafiyu-09-23)'
+    bot.send_message(message.chat.id, link, parse_mode='MarkdownV2')
 	
 	
 @bot.message_handler(commands=["admins"])
 def send_rules(message):
-    	link = '[Как стать адмиином? Посмотрите статью](https://telegra.ph/Kak-stat-adminom-v-Mafia-Bonds-09-27)'
-    	bot.send_message(message.chat.id, link, parse_mode='MarkdownV2')
+    link = '[Как стать адмиином? Посмотрите статью](https://telegra.ph/Kak-stat-adminom-v-Mafia-Bonds-09-27)'
+    bot.send_message(message.chat.id, link, parse_mode='MarkdownV2')
 
                                
 @bot.message_handler(commands=["help"])
 def send_rules(message):
-    	bot.send_message(message.chat.id, 'Доступные команды:\n /rules - Показывает ссылку на правила этого чата\n /faq - Показывает ссылку на статью как играть в мафию\n /id - Показывает айди вашего профиля\n /admins - Как стать админом?\n')
+    bot.send_message(message.chat.id, 'Доступные команды:\n /rules - Показывает ссылку на правила этого чата\n /faq - Показывает ссылку на статью как играть в мафию\n /id - Показывает айди вашего профиля\n /admins - Как стать админом?\n')
     
 @bot.message_handler(commands=['id'])
 def send_start(message):
-	bot.send_message(message.chat.id, 'Твой ID: ' + str(message.from_user.id))
+    bot.send_message(message.chat.id, 'Твой ID: ' + str(message.from_user.id))
 
 '''
 -----------------------------------------------------------------------------------------
