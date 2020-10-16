5e33b9f8ba38294cdc71b13d2ee57435f43b30ed
Ram2020 <65360179+ramdev-git@users.noreply.github.com>
2020-10-16 20:47:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ def handle_docs_audio(message):
     keyboard = types.InlineKeyboardMarkup()
     url_button = types.InlineKeyboardButton(text="Перейти на Яндекс", url="https://ya.ru")
     keyboard.add(url_button)
-    bot.send_message(message.chat.id, "🙋Добро пожаловать, {0.first_name}!\n⚙️Доступные команды можно узнать через /help".format(message.from_user, bot.get_me()) reply_markup=keyboard)
+    bot.send_message(message.chat.id, "🙋Добро пожаловать, {0.first_name}!\n⚙️Доступные команды можно узнать через /help".format(message.from_user, bot.get_me()), ,reply_markup=keyboard)
     
 @bot.message_handler(commands=["rules"])
 def send_rules(message):
