d5c2a1db182ef3e864c497825b226f7b1f04f39d
Ram2020 <65360179+ramdev-git@users.noreply.github.com>
2020-11-01 11:29:04 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,11 @@
-
-
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram import *
 from aiogram.types import *
 
-# 1472805654:AAHTOeNeusZOSqE4eMC7J66lb0v4Dqyyoz0 token
+
 TOKEN = "1472805654:AAHTOeNeusZOSqE4eMC7J66lb0v4Dqyyoz0"
 admin_id = 1355949068
+admin_id = 1383795967
 
 
 boty = Bot(token=TOKEN)
@@ -15,63 +14,42 @@ dp = Dispatcher(boty)
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-	if message['from'].id == admin_id:
-		await message.answer(f"Привет Админ!")
-	else:
-		await message.answer(f"Привет, {message['from'].first_name}! Вас приветствует Mafia Bonds")
+  if message['from'].id == admin_id:
+    await message.answer(f"Привет Админ!")
+  else:
+    await message.answer(f"Привет, {message['from'].first_name}! Бот для         обратной связи. Напиши и мы скоро ответим")
 
 
 @dp.message_handler(commands=['help'])
-async def group_faq(message:types.Message):
-	await message.answer(f'Доступные команды:\n /rules - Показывает ссылку на правила этого чата\n /faq - Показывает ссылку на статью как играть в мафию\n /admin - Как стать админом?\n')
+async def send_help(message: types.Message):
+  await message.answer('@Mrfire7')
 
-
-@dp.message_handler(commands=['rules'])
-async def group_rules(message: types.Message):
-	rules_inl_keyb = InlineKeyboardMarkup()
-	rules_inl_keyb.add(InlineKeyboardButton('Правила чата', url='https://telegra.ph/Pravila-igry-09-08-4'))
-	await message.answer(f"Наши правила:", reply_markup=rules_inl_keyb)
-
-
-@dp.message_handler(commands=['faq'])
-async def group_faq(message:types.Message):
-	faq_inl_keyb = InlineKeyboardMarkup()
-	faq_inl_keyb.add(InlineKeyboardButton('FAQ', url='https://telegra.ph/FAQ-po-igre-v-Mafiyu-09-23'))
-	await message.answer(f'Справка', reply_markup=faq_inl_keyb)
-
-
-@dp.message_handler(commands=['admin'])
-async def how_to_be_admin(message:types.Message):
-	adm_inl_keyb = InlineKeyboardMarkup()
-	adm_inl_keyb.add(InlineKeyboardButton('Статья', url='https://telegra.ph/Kak-stat-adminom-v-Mafia-Bonds-09-27'))
-	await message.answer(f'Как стать адмиином? Посмотрите статью', reply_markup=adm_inl_keyb)
-
-
-		
+    
 @dp.message_handler()
 async def process_start_command(message: types.Message):
-	if message.reply_to_message == None:
-		if '/start' not in message.text:
-			await boty.forward_message(admin_id, message.from_user.id, message.message_id)
+  if message.reply_to_message == None:
+    if '/start' not in message.text:
+      await boty.forward_message(admin_id, message.from_user.id,           message.message_id)
+    elif '/help' not in message.text:
+      await boty.forward_message(admin_id, message.from_user.id,           message.message_id)
+  else:
+    if message['from'].id == admin_id:
+      if message.reply_to_message.forward_from.id:
+        await boty.send_message(message.reply_to_message.forward_from.id,             message.text)
+    else:
+      await message.answer('Нельзя отвечать на сообщения.')
 
 
-	else:
-		if message['from'].id == admin_id:
-			if message.reply_to_message.forward_from.id:
-				await boty.send_message(message.reply_to_message.forward_from.id, message.text)
-		else:
-			await message.answer('Нельзя отвечать на сообщения.')
-
-			
+      
 @dp.message_handler(content_types=['photo'])
 async def handle_docs_photo(message):
-	await boty.forward_message(admin_id, message.from_user.id, message.message_id)
+  await boty.forward_message(admin_id, message.from_user.id, message.message_id)
 
-	
+  
 @dp.message_handler(content_types=['document'])
 async def handle_docs_photo(message):
-	await boty.forward_message(admin_id, message.from_user.id, message.message_id)
+  await boty.forward_message(admin_id, message.from_user.id, message.message_id)
 
-	
+  
 if __name__ == '__main__':
-	executor.start_polling(dp)
+  executor.start_polling(dp)
